3b9ef7a07e370d6c18c88d984128f260422dc8ca
InduBhusanNath <ibnhost@gmail.com>
2024-10-17 13:11:12 +0530

    Th

--- express-app/server.js
+++ express-app/server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var server=express();
 var routes=require('./routes/routes');
 server.use('/',routes);
-server.listen(5000,()=>{
+server.listen(5000,'0.0.0.0',()=>{
     console.log('Application Started At Port 5000');
 })
