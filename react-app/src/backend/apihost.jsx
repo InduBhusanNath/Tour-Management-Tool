d89540ab6e4a12f50f23cc2b052b0fe6c1f63e12
InduBhusanNath <ibnhost@gmail.com>
2025-01-01 11:52:35 +0530

    New Year Push

--- react-app/src/backend/apihost.jsx
+++ react-app/src/backend/apihost.jsx
@@ -1,3 +1,3 @@
-export const apihost='http://localhost:5000/api';
-//export const apihost='https://aeiety.com/api';
+//export const apihost='http://localhost:5000/api';
+export const apihost='https://aeiety.com/api';
 
