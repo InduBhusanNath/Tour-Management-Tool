29e73afa1c9d7d8dc48730ab740d32a1dcfbb32c
InduBhusanNath <ibnhost@gmail.com>
2025-08-03 11:32:17 +0530

    Aug 1st Push

--- react-app/src/frontend/scuba-diving.jsx
+++ react-app/src/frontend/scuba-diving.jsx
@@ -1,6 +1,7 @@
 import Header from "./header";
 import Footer from "./footer";
 import { Helmet, HelmetProvider } from 'react-helmet-async';
+import Accordion from 'react-bootstrap/Accordion';
 
 //Export Function
 export default function ScubaDiving(){
@@ -15,6 +16,7 @@ export default function ScubaDiving(){
                  <Intro/>
                  <DivingGear/>
                  <DivingBenefits/>
+                 <FAQs/>
                  <Footer/>
              </div>
          </HelmetProvider>     
@@ -42,7 +44,7 @@ function ScubaDivingBanner(){
   return(<>
        <div className="row">
             <div className="col-sm-12 p-0 m-0">
-                 <img src="https://i.postimg.cc/BvwmFK2y/scubabanner.webp" alt="Our Scuba Diving in India Packages" className="w-100 img-fluid p-0 m-0"/>
+                 <img src="https://i.postimg.cc/v8gyhBWw/scubabanner.webp" alt="Our Scuba Diving in India Packages" className="w-100 img-fluid p-0 m-0"/>
             </div>
        </div>
   </>);
@@ -51,7 +53,7 @@ function ScubaDivingBanner(){
 function BreadCrumb(){
   return(<>
             <div className="row">
-                 <div className="col-sm-12 background-lightblue">
+                 <div className="col-sm-12 background-whitesmoke">
                       <nav aria-label="breadcrumb">
                            <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a href="/">Home</a></li>  
@@ -67,7 +69,7 @@ function BreadCrumb(){
 //H1
 function H1(){
       return(<>
-           <div className="row background-lightblue p-3">
+           <div className="row background-whitesmoke p-3">
                 <div className="col-sm-1"></div>
                 <div className="col-sm-10">
                      <h1 className="text-center"> Plunge into Scuba Diving in India With Our Packages</h1>
@@ -79,7 +81,7 @@ function H1(){
 //Intro
 function Intro(){
      return(<>
-           <div className="row background-lightblue p-3">
+           <div className="row background-whitesmoke p-3">
                 <div className="col-sm-1"></div>
                 <div className="col-sm-10">
                      <p className="font font22">
@@ -88,7 +90,17 @@ function Intro(){
                      <p className="font font22">
                           It is appealing, educational, and benefits health. As such, there is an all-round result from the activity. Understandably, the activity involves diving into the sea or ocean.
                      </p>
-                                         
+                     <p className="font font22">
+                          Underwater diving also involves wearing protective gear. It is a safe activity. Also, practice can help to master the activity quickly.
+                     </p>
+                     <p className="font font22">
+                          Expert professionals at Aeiety Trips can streamline your scuba dive. They will guide you to experience the best dive. Their guidance will eliminate your fear, if you have any. 
+                     </p>
+                     <p className="font font22">
+                          We provide excellent packages for scuba diving in India at affordable prices. The packages correspond to key locations in the country. Choose our packages to undergo a memorable diving experience. 
+                     </p>
+
+
                 </div>
                 <div className="col-sm-1"></div>
            </div>
@@ -97,14 +109,14 @@ function Intro(){
 //Diving Gear
 function DivingGear(){
      return(<>
-           <div className="row background-lightblue p-3">
+           <div className="row background-ghostwhite p-3">
                 <div className="col-sm-1"></div>
                 <div className="col-sm-10">
                      <h2>Underwater Diving: A Breakdown of Protective Gear</h2>
                 </div>
                 <div className="col-sm-1"></div>
            </div>
-           <div className="row background-lightblue p-3">
+           <div className="row background-ghostwhite p-3">
                 <div className="col-sm-1"></div>
                 <div className="col-sm-10">
                      <p className="font font22">
@@ -131,14 +143,14 @@ function DivingGear(){
 //Diving Benefits
 function DivingBenefits(){
       return(<>
-           <div className="row background-lightblue p-3">
+           <div className="row background-whitesmoke p-3">
                 <div className="col-sm-1"></div>
                 <div className="col-sm-10">
                      <h2>Scuba Diving in India: Its Key Benefits</h2>
                 </div>
                 <div className="col-sm-1"></div>
            </div>
-           <div className="row background-lightblue p-3">
+           <div className="row background-whitesmoke p-3">
                 <div className="col-sm-1"></div>
                 <div className="col-sm-10">
                      <p className="font font22">
@@ -152,7 +164,7 @@ function DivingBenefits(){
                           <ul>
                                <li>An hour of diving can burn 390 to 788 calories of body fat. Continuous body movements act as an exercise to burn fat. As such, underwater diving can help to lose weight.</li>
                                <li>Physical activities underwater strengthen muscles and tone the body.</li>
-                               <li>Slow and deep breaths with air from the apparatus improve breathing control and respiratory health. Also, a scuba dive can lead to a balanced heart rate. </li>
+                               <li>Slow and deep breaths with air from the apparatus improve breathing control and respiratory health. Also, a scuba dive can stabilize heart rate.</li>
                                <li>The water environment lowers blood pressure. As such, a session of scuba diving in India can instill calmness in the body.</li>
                                <li>Movements of different body parts improve blood circulation.</li>
                                <li>Sea or ocean water improves skin health.</li>
@@ -162,7 +174,7 @@ function DivingBenefits(){
                      <p className="font font22">
                           <ul>
                                <li>The environment inside the sea or ocean is captivating and thought-provoking. Also, it causes peace of mind. As such, underwater diving reduces stress.</li>
-                               <li>Smoothly moving from one underwater spot to another is a kind of problem-solving. Underwater movement necessitates good decision-making. As such, scuba dives build confidence and self-esteem in the diver.</li>
+                               <li>Navigating underwater is akin to problem-solving. Underwater movement necessitates good decision-making. As such, scuba dives build confidence and self-esteem in the diver.</li>
                                <li>The sight of vegetation, marine species, coral reefs and other underwater entities can create broader perspectives on nature. As such, scuba diving in India can create a sense of bonding with nature.</li>
                           </ul>
                      </p>
@@ -170,12 +182,57 @@ function DivingBenefits(){
                      <p className="font font22">
                           <ul>
                                <li>Meeting various people can entail exchanging ideas. That could be a significant benefit from participating in a scuba dive.</li>
-                               <li>Interacting with people of different cultures will allow understanding different facets of their cultures. Taking part in underwater diving entails substantial social benefits.</li>
+                               <li>Interacting with people belonging to different cultures will allow understanding various cultural facets. Taking part in underwater diving entails substantial social benefits.</li>
                                <li>Scuba diving in India involves interacting with fellow divers. That improves communication skills. Also, the friend circle increases. </li>
                           </ul>
                      </p>                  
                 </div>
                 <div className="col-sm-1"></div>
            </div>
+      </>);
+}
+//Faqs
+function FAQs(){
+      return(<>
+           <div className="row background-ghostwhite p-3">
+                <div className="col-sm-1"></div>
+                <div className="col-sm-10">
+                     <h2>FAQs: Underwater Diving</h2>
+                </div>
+                <div className="col-sm-1"></div>
+           </div>
+           <div className="row background-ghostwhite p-3">
+                <div className="col-sm-2"></div>
+                <div className="col-sm-8">
+                    <Accordion flush>
+                          <Accordion.Item eventKey="0">
+                               <Accordion.Header>
+                                    <span className="font font22">
+                                         #1. How can I learn about scuba diving in India?
+                                    </span>
+                               </Accordion.Header>                                    
+                               <Accordion.Body>
+                                    <p className="font font22">
+                                         Read this page. It covers the essential information for a holistic understanding. 
+                                    </p>
+                               </Accordion.Body>
+                          </Accordion.Item>
+                          <Accordion.Item eventKey="1">
+                               <Accordion.Header>
+                                    <span className="font font22">
+                                         #2. Is the cost of scuba diving high?
+                                    </span>
+                               </Accordion.Header>                                    
+                               <Accordion.Body>
+                                    <p className="font font22">
+                                        No, it is not. The cost depends on operator to operator. Costs at Aeiety Trips are affordable. 
+                                    </p>
+                               </Accordion.Body> 
+                          </Accordion.Item> 
+                                               
+                     </Accordion>                   
+                </div>
+                <div className="col-sm-2"></div>
+           </div>      
       </>);
 }
